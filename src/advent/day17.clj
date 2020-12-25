65462ca17a224b9dea760eb23733c24e784a0ba2
Steve Yu <suguni@gmail.com>
2020-12-25 22:42:05 +0900

    size to dim

--- src/advent/day17.clj
+++ src/advent/day17.clj
@@ -2,60 +2,6 @@
   (:require
             [clojure.string :as str]))
 
-(def input (str (repeat-str (* 8 8 3) \.) (.replaceAll "
-..##.#.#
-.#####..
-#.....##
-##.##.#.
-..#...#.
-.#..##..
-.#...#.#
-#..##.##
-" "\\s+" "") (repeat-str (* 8 8 4) \.)))
-
-(def c0 "..........#...####.........")
-(def c1 "#....#.#.#.#.##.#.#....#.#.")
-(def c2 (.replaceAll "
-.....
-.....
-..#..
-.....
-.....
-
-..#..
-.#..#
-....#
-.#...
-.....
-
-##...
-##...
-#....
-....#
-.###.
-
-..#..
-.#..#
-....#
-.#...
-.....
-
-.....
-.....
-..#..
-.....
-.....
-" "\\s+" ""))
-
-
-(defn update-cell [state active-neighbors]
-  (case state
-    \# (if (or (= 2 active-neighbors)
-               (= 3 active-neighbors)) \# \.)
-    \. (if (= active-neighbors 3) \# \.)))
-
-(update-cell \. 3)
-
 (defn repeat-str [n s]
   (->> s
        (repeat n)
@@ -66,57 +12,82 @@
        (partition n)
        (map #(apply str %))))
 
-(defn pad-slice [slice size n]
+(def s "
+..##.#.#
+.#####..
+#.....##
+##.##.#.
+..#...#.
+.#..##..
+.#...#.#
+#..##.##
+")
+
+(def e "
+#..
+..#
+.#.
+
+#.#
+.##
+.#.
+
+#..
+..#
+.#.
+")
+
+(defn remove-sps [s]
+  (.replaceAll s "\\s+" ""))
+
+(defn update-cell [state active-neighbors]
+  (case state
+    \# (if (or (= 2 active-neighbors)
+               (= 3 active-neighbors)) \# \.)
+    \. (if (= active-neighbors 3) \# \.)))
+
+(defn pad-plane [[sx _] slice n]
   (let [pad-line (->> \.
-                      (repeat-str (+ size (* n 2)))
+                      (repeat-str (+ sx (* n 2)))
                       (repeat-str n))
         pad-side (repeat-str n \.)
         padded-slice (->> slice
-                          (partition-str size)
+                          (partition-str sx)
                           (map #(str pad-side % pad-side))
                           str/join)]
     (str pad-line padded-slice pad-line)))
 
-(defn pad-cube [cube size n]
-  (let [padded-size (+ size (* n 2))
-        plane (repeat-str (* padded-size padded-size n) \.)
+(defn pad-cube [[sx sy _ :as dim] cube n]
+  (let [[psx psy _] (map #(+ % (* n 2)) dim)
+        plane (repeat-str (* psx psy n) \.)
         padded-cube (->> cube
-                         (partition-str (* size size))
-                         (map #(pad-slice % size n))
+                         (partition-str (* sx sy))
+                         (map #(pad-plane [sx sy] % n))
                          str/join)]
     (str plane padded-cube plane)))
 
-(pad-cube "x" 1 2)
-(pad-slice "....*...." 3 2)
-
-(defn cube-neighbors [size]
-  (let [stride (* size size)
+(defn cube-neighbors [[sx sy _]]
+  (let [stride (* sx sy)
         z=0 (for [r [-1 0 1]
-                  c [-1 0 1]] (+ (* r size) c))
+                  c [-1 0 1]] (+ (* r sx) c))
         z-1 (map #(- % stride) z=0)
         z+1 (map #(+ % stride) z=0)]
     (filter #(not (zero? %)) (concat z-1 z=0 z+1))))
 
-(cube-neighbors 5)
+(defn cube-coords [[sx sy sz]]
+  (for [z (range 1 (dec sz))
+        y (range 1 (dec sy))
+        x (range 1 (dec sx))]
+    (+ (* z (* sx sy)) (* y sx) x)))
 
-(defn cube-size [cube]
-  (Math/round (Math/pow (.length cube) (/ 1 3))))
-
-(defn cube-coords [size]
-  (let [stride (* size size)]
-    (for [z (range 1 (dec size))
-          y (range 1 (dec size))
-          x (range 1 (dec size))]
-      (+ (* z stride) (* y size) x))))
-
-(defn generate [cube]
-  (let [size (cube-size cube)
-        padded (pad-cube cube size 2)   ; 7x7x7
-        coords (cube-coords (+ size 2 2))
-        neighbors (cube-neighbors (+ size 2 2))
+(defn generate [dim cube]
+  (let [padded (pad-cube dim cube 2)    ; 7x7x7
+        padded-dim (map #(+ 4 %) dim)
+        coords (cube-coords padded-dim)
+        neighbors (cube-neighbors padded-dim)
         active-counts (fn [i] (->> neighbors
                                    (filter #(= \# (.charAt padded (+ % i))))
-                                   (map #(+ i %))
+                                   ;; (map #(+ i %))
                                    count))]
     (->> coords
          (map (fn [c]
@@ -125,63 +96,19 @@
                   (update-cell cell actives))))
          (apply str))))
 
-(->> input
-     generate
-     generate
-     generate
-     generate
-     generate
-     generate
-     ;; .length
-     (filter #(= % \#))
-     count)
+(defn evolve [dim input n]
+  (loop [input (.replaceAll input "\\s+" "")
+         dim dim
+         i 0]
+    (if (= i n)
+      (->> input
+           (filter #(= % \#))
+           count)
+      (recur (generate dim input)
+             (map #(+ % 2) dim)
+             (inc i)))))
+
+(evolve [3 3 3] e 5)
+(evolve [8 8 1] s 6)
 
 ;;;  213
-
-
-(pad-cube c1 3 0)
-"
-...
-...
-...
-
-...
-.#.
-...
-
-..#
-...
-#..
-
-...
-...
-.#.
-
-#...##...#.........#......#...#.............."
-
-"
-.......
-.......
-.......
-.......
-.......
-.......
-.......
-
-.......
-.#.....
-.#...#.
-.......
-.......
-..#.#..
-.##...#
-
-.......
-.......
-...#...
-...#...
-#......
-.......
-.......
-
-......................................"
