2ad592b8097e720e7e79404dfc31e2528ade5bc2
Steve Yu <suguni@gmail.com>
2021-08-30 09:24:49 +0900

    2015 day 15 part One, Two, 일단 정답 제출

--- advent_rs/src/y2015/d15.rs
+++ advent_rs/src/y2015/d15.rs
@@ -1,9 +1,10 @@
 use regex::Regex;
+use super::*;
 
+#[derive(PartialEq, Eq, Debug)]
 struct Ingredient(i32, i32, i32, i32, i32);
 
 fn parse_line(line: &str) -> Ingredient {
-
     let re = Regex::new(
         r"\w+: capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)")
         .unwrap();
@@ -25,25 +26,95 @@ fn load_ingredients(text: &str) -> Vec<Ingredient> {
         .collect()
 }
 
-// fn combinations(value: i32, slots: usize) -> usize {
-//     if slots == 1 {
-//         1
-//     } else {
-//         for i in 0..=value {
-//             combinations(i, slots - 1);
-//         }
-//
-//     }
-// }
+fn calc_score(ingredients: &Vec<Ingredient>, weights: Vec<i32>) -> (i32, i32) {
+    let mut sum = [0; 4];
+    let mut cal = 0;
+    for i in 0..ingredients.len() {
+        sum[0] += ingredients[i].0 * weights[i];
+        sum[1] += ingredients[i].1 * weights[i];
+        sum[2] += ingredients[i].2 * weights[i];
+        sum[3] += ingredients[i].3 * weights[i];
+        cal += ingredients[i].4 * weights[i];
+    }
+
+    (
+        sum[0].max(0) * sum[1].max(0) * sum[2].max(0) * sum[3].max(0),
+        cal
+    )
+
+}
+
+fn max_score(ingredients: &Vec<Ingredient>) -> i32 {
+    let mut max = 0;
+
+    for a in 0..100 {
+        for b in 0..100 {
+            for c in 0..100 {
+                for d in 0..100 {
+                    if a + b + c + d == 100 {
+                        let (c, _cal) = calc_score(ingredients, vec![a, b, c, d]);
+                        if c > max {
+                            max = c;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    max
+}
+
+fn max_score2(ingredients: &Vec<Ingredient>) -> i32 {
+    let mut max = 0;
+
+    for a in 0..100 {
+        for b in 0..100 {
+            for c in 0..100 {
+                for d in 0..100 {
+                    if a + b + c + d == 100 {
+                        let (c, cal) = calc_score(ingredients, vec![a, b, c, d]);
+                        if cal == 500 && c > max {
+                            max = c;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    max
+}
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
 
     #[test]
     fn test_parse() {
         assert_eq!(parse_line("Frosting: capacity 4, durability -2, flavor 0, texture 0, calories 5"),
-                   (4, -2, 0, 0, 5));
+                   Ingredient(4, -2, 0, 0, 5));
+    }
+
+    #[test]
+    fn quiz1() {
+        let ingredients: Vec<Ingredient> =
+            read_file("../data/2015/input15.txt")
+                .lines()
+                .map(|line| parse_line(line))
+                .collect();
+
+        assert_eq!(max_score(&ingredients), 18965440);
+    }
+
+    #[test]
+    fn quiz2() {
+        let ingredients: Vec<Ingredient> =
+            read_file("../data/2015/input15.txt")
+                .lines()
+                .map(|line| parse_line(line))
+                .collect();
+
+        assert_eq!(max_score2(&ingredients), 15862900);
     }
 }
