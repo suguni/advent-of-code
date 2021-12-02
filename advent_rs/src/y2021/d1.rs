e17bdd558d300b6a2b5dbde91c43c020361b8562
Steve Yu <suguni@gmail.com>
2021-12-02 19:53:17 +0900

    complet 2021 day 1

--- advent_rs/src/y2021/d1.rs
+++ advent_rs/src/y2021/d1.rs
@@ -1,12 +1,28 @@
 use crate::read_file;
 
 pub fn quiz1() -> usize {
-    let text = read_file("data/2021/input1.txt");
-    let nums: Vec<i32> = text.lines().map(|l| l.parse::<i32>().unwrap()).collect();
+    let nums = load("data/2021/input1.txt");
     count_increased(nums)
 }
 
-pub fn count_increased(nums: Vec<i32>) -> usize {
+pub fn quiz2() -> usize {
+    let nums = load("data/2021/input1.txt");
+    let sums = moving_windows(nums, 3);
+    count_increased(sums)
+}
+
+fn load(filename: &str) -> Vec<i32> {
+    let text = read_file(filename);
+    text.lines().map(|l| l.parse::<i32>().unwrap()).collect()
+}
+
+fn moving_windows(nums: Vec<i32>, size: usize) -> Vec<i32> {
+    nums.windows(size)
+        .map(|ns| ns.iter().sum())
+        .collect::<Vec<i32>>()
+}
+
+fn count_increased(nums: Vec<i32>) -> usize {
     nums.windows(2)
         .map(|ps| ps[0] < ps[1])
         .filter(|&b| b)
@@ -25,7 +41,21 @@ mod tests {
     }
 
     #[test]
+    fn test_moving_windows() {
+        let nums = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        assert_eq!(
+            moving_windows(nums, 3),
+            [607, 618, 618, 617, 647, 716, 769, 792]
+        );
+    }
+
+    // #[test]
     fn run_quiz1() {
         assert_eq!(quiz1(), 0);
+    }
+
+    // #[test]
+    fn run_quiz2() {
+        assert_eq!(quiz2(), 0);
     }
 }
