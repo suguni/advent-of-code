e8678dbf356b821a620f301137cab4f34d9d289b
steve.yu <suguni@gmail.com>
2021-12-06 18:23:43 +0900

    2021 day6. 조금 최적화 했지만 이걸로 안됨.

--- advent_rs/src/y2021/d6.rs
+++ advent_rs/src/y2021/d6.rs
@@ -1,7 +1,8 @@
-pub fn tick(state: &mut Vec<i32>) {
+pub fn tick(state: &mut Vec<i32>) -> usize {
+    let min = *state.iter().min().unwrap();
     let mut new_generation = vec![];
     for s in state.iter_mut() {
-        *s -= 1;
+        *s -= min + 1;
         if *s < 0 {
             *s = 6;
             new_generation.push(8);
@@ -11,16 +12,62 @@ pub fn tick(state: &mut Vec<i32>) {
     if !new_generation.is_empty() {
         state.append(&mut new_generation);
     }
+
+    (min + 1) as usize
 }
 
 pub fn load_data(text: &str) -> Vec<i32> {
     text.split(',').map(|s| s.parse().unwrap()).collect()
 }
 
-pub fn play(state: &mut Vec<i32>, count: usize) -> usize {
-    (0..count).for_each(|_| tick(state));
+pub fn play(state: &mut Vec<i32>, mut count: usize) -> usize {
+    while count != 0 {
+        count -= tick(state);
+        println!("{}", count);
+    }
     state.len()
 }
+
+// pub fn jump(state: i32) -> Vec<i32> {
+//     vec![6, 8]
+// }
+
+// pub fn jumps(state: &mut Vec<i32>) {
+//     let min = state.iter().min();
+//     for s in state.iter_mut() {
+//         *s -= (min + 1);
+//         if *s < 0 {
+//             *s = 6;
+//         }
+//     }
+//     state.map(|v| v - min - 1);
+
+// }
+
+// 3
+
+// 2
+// 1
+// 0
+// 6 8
+// 5 7
+// 4 6
+// 3 5
+// 2 4
+// 1 3
+// 0 2
+// 6 1 8
+// 5 0 7
+// 4 6 6 8
+// 3 5 5 7
+// 2 4 4 6
+// 1 3 3 5
+// 0 2 2 4
+// 6 1 1 3
+
+// 6 5 4 3 2 1 0
+// 6 5 4 3 2 1 0
+// 6 5 4 3
 
 // pub fn calc(state: i32, mut days: usize, cycle: usize) -> usize {
 //     days -= state;
@@ -53,9 +100,6 @@ mod tests {
     #[test]
     fn test_one_tick() {
         let mut state = vec![3, 4, 3, 1, 2];
-
-        tick(&mut state);
-        assert_eq!(state, [2, 3, 2, 0, 1]);
 
         tick(&mut state);
         assert_eq!(state, [1, 2, 1, 6, 0, 8]);
