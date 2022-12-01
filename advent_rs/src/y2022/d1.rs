9ec18141c028008270f47b3069ecc7cce73a2598
Steve Yu <suguni@gmail.com>
2022-12-01 22:34:45 +0900

    update y2022 d1

--- advent_rs/src/y2022/d1.rs
+++ advent_rs/src/y2022/d1.rs
@@ -3,43 +3,34 @@ use crate::read_file;
 
 fn quiz1() -> i32 {
     let text = read_file("data/2022/input1.txt");
-    most_calories(text)
+    most_calories(&text)
 }
 
 fn quiz2() -> i32 {
     let text = read_file("data/2022/input1.txt");
-    top_three_calories(text)
+    top_three_calories(&text)
 }
 
-fn most_calories(data: String) -> i32 {
+fn most_calories(data: &str) -> i32 {
     let calories = load(data);
     *calories.iter().max().unwrap()
 }
 
-fn top_three_calories(data: String) -> i32 {
+fn top_three_calories(data: &str) -> i32 {
     let mut calories = load(data);
     calories.sort_by(|a, b| b.cmp(a));
     calories[0] + calories[1] + calories[2]
 }
 
-fn load(data: String) -> Vec<i32> {
-    let mut calories = Vec::new();
-    let mut s = 0;
-
-    for line in data.lines() {
-        if line.is_empty() {
-            calories.push(s);
-            s = 0;
-        } else {
-            s += line.parse::<i32>().unwrap();
-        }
-    }
-
-    if s != 0 {
-        calories.push(s);
-    }
-
-    calories
+fn load(data: &str) -> Vec<i32> {
+    data.split("\n\n")
+        .map(|block| {
+            block
+                .lines()
+                .map(|line| line.parse::<i32>().unwrap())
+                .sum::<i32>()
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -63,8 +54,8 @@ mod tests {
 9000
 
 10000";
-        assert_eq!(most_calories(data.clone().to_owned()), 24000);
-        assert_eq!(top_three_calories(data.to_owned()), 45000);
+        assert_eq!(most_calories(data), 24000);
+        assert_eq!(top_three_calories(data), 45000);
     }
 
     #[test]
