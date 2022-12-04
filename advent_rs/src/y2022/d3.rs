af8fbb1eee554308917f2b1d90fe62bc07127e32
Steve Yu <suguni@gmail.com>
2022-12-04 11:52:59 +0900

    refactor y2022 day3 - https://www.youtube.com/watch?v=yBJJYkC5cdk&t=468s

--- advent_rs/src/y2022/d3.rs
+++ advent_rs/src/y2022/d3.rs
@@ -1,28 +1,28 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use crate::read_file;
 
+fn letter_scores() -> HashMap<char, usize> {
+    ('a'..='z')
+        .chain(('A'..='Z'))
+        .enumerate()
+        .map(|(idx, letter)| (letter, idx + 1))
+        .collect()
+}
+
 fn sum_priorities(input: &str) -> i32 {
+    let letter_scores = letter_scores();
+
     input
         .lines()
         .map(|line| {
-            let len = line.chars().count();
-            let (v1, v2): (Vec<_>, Vec<_>) =
-                line.chars().enumerate().partition(|(i, _)| *i < len / 2);
-            let s1: HashSet<_> = v1.iter().map(|(_, c)| c).collect();
-            let s2: HashSet<_> = v2.iter().map(|(_, c)| c).collect();
-            let p = **s1.intersection(&s2).next().unwrap();
-            priority(p)
+            let len = line.len();
+            let v1 = &line[..len / 2];
+            let v2 = &line[len / 2..];
+            let found = v1.chars().find(|c| v2.contains(*c)).unwrap();
+            letter_scores.get(&found).unwrap()
         })
-        .sum()
-}
-
-fn priority(p: char) -> i32 {
-    if p.is_uppercase() {
-        (p as u8 - 'A' as u8) as i32 + 27
-    } else {
-        (p as u8 - 'a' as u8) as i32 + 1
-    }
+        .sum::<usize>() as i32
 }
 
 fn quiz1() -> i32 {
@@ -31,6 +31,7 @@ fn quiz1() -> i32 {
 }
 
 fn sum_priorities2(input: &str) -> i32 {
+    let letter_scores = letter_scores();
     input
         .lines()
         .enumerate()
@@ -39,19 +40,18 @@ fn sum_priorities2(input: &str) -> i32 {
                 acc.push(Vec::new());
             }
             let i = acc.len() - 1;
-            acc[i].push(item.chars().collect::<HashSet<char>>());
+            acc[i].push(item);
             acc
         })
         .iter()
         .map(|ls| {
-            let is1 = ls[0]
-                .intersection(&ls[1])
-                .map(|c| *c)
-                .collect::<HashSet<_>>();
-            let p = *is1.intersection(&ls[2]).next().unwrap();
-            priority(p)
+            let found = ls[0]
+                .chars()
+                .find(|c| ls[1].contains(*c) && ls[2].contains(*c))
+                .unwrap();
+            letter_scores.get(&found).unwrap()
         })
-        .sum()
+        .sum::<usize>() as i32
 }
 
 fn quiz2() -> i32 {
