16408ccebb862d02872cde9c8658a3863c22a3a6
YuSeungKeun <suguni@gmail.com>
2025-12-05 22:07:09 +0900

    2025 day 5 - range -> tuple

--- advent_rs/src/y2025/d5.rs
+++ advent_rs/src/y2025/d5.rs
@@ -5,8 +5,6 @@ use nom::character::complete::newline;
 use nom::multi::separated_list1;
 use nom::sequence::separated_pair;
 use nom::{IResult, Parser};
-use std::collections::VecDeque;
-use std::ops::RangeInclusive;
 
 const QUIZ_INPUT: &str = include_str!("../../data/2025/input5.txt");
 
@@ -22,7 +20,7 @@ fn solve1(data: &str) -> u64 {
     let (_, data) = parse_data(data).unwrap();
     let mut count = 0;
     for id in data.ids {
-        if data.ranges.iter().find(|&r| r.contains(&id)).is_some() {
+        if data.ranges.iter().find(|(l, r)| id >= *l && id <= *r).is_some() {
             count += 1;
         }
     }
@@ -31,17 +29,17 @@ fn solve1(data: &str) -> u64 {
 
 fn solve2(data: &str) -> u64 {
     let (_, mut data) = parse_data(data).unwrap();
-    let mut merged_ranges: VecDeque<RangeInclusive<u64>> = VecDeque::new();
+    let mut merged_ranges: Vec<(u64, u64)> = vec![];
 
-    data.ranges.sort_by(|r1, r2| r1.start().cmp(r2.start()));
+    data.ranges.sort_by(|(l1, _), (l2, _)| l1.cmp(l2));
 
     let mut i = 0;
     let mut j = 1;
     while i < data.ranges.len() - 1 {
-        let mut r1 = *data.ranges[i].start()..=*data.ranges[i].end();
+        let mut r1 = data.ranges[i].clone();
         j = i + 1;
         while j < data.ranges.len() {
-            let r2 = *data.ranges[j].start()..=*data.ranges[j].end();
+            let r2 = data.ranges[j];
             if intersect(&r1, &r2) {
                 r1 = merge(&r1, &r2);
                 j += 1;
@@ -50,27 +48,27 @@ fn solve2(data: &str) -> u64 {
             }
         }
         i = j;
-        merged_ranges.push_back(r1);
+        merged_ranges.push(r1);
     }
 
     merged_ranges.into_iter()
-        .map(|r| r.count() as u64)
+        .map(|(s, e)| e - s + 1)
         .sum()
 }
 
-fn intersect(r1: &RangeInclusive<u64>, r2: &RangeInclusive<u64>) -> bool {
-    !(r1.end() < r2.start() || r2.end() < r1.start())
+fn intersect(r1: &(u64, u64), r2: &(u64, u64)) -> bool {
+    !(r1.1 < r2.0 || r2.1 < r1.0)
 }
 
-fn merge(r1: &RangeInclusive<u64>, r2: &RangeInclusive<u64>) -> RangeInclusive<u64> {
-    *r1.start().min(r2.start())..=*r1.end().max(r2.end())
+fn merge(r1: &(u64, u64), r2: &(u64, u64)) -> (u64, u64) {
+    (r1.0.min(r2.0), r1.1.max(r2.1))
 }
 
 fn parse_data(input: &str) -> IResult<&str, Data> {
     separated_pair(
         separated_list1(
             newline,
-            separated_pair(complete::u64, tag("-"), complete::u64).map(|(a, b)| a..=b),
+            separated_pair(complete::u64, tag("-"), complete::u64).map(|(a, b)| (a, b)),
         ),
         (newline, newline),
         separated_list1(newline, complete::u64),
@@ -80,7 +78,7 @@ fn parse_data(input: &str) -> IResult<&str, Data> {
 }
 
 struct Data {
-    ranges: Vec<RangeInclusive<u64>>,
+    ranges: Vec<(u64, u64)>,
     ids: Vec<u64>,
 }
 
@@ -103,7 +101,7 @@ mod tests {
     #[test]
     fn test_parse() {
         let (_, data) = parse_data(SAMPLE).unwrap();
-        assert_eq!(data.ranges, vec![3..=5, 10..=14, 16..=20, 12..=18]);
+        assert_eq!(data.ranges, vec![(3, 5), (10, 14), (16, 20), (12, 18)]);
         assert_eq!(data.ids, vec![1, 5, 8, 11, 17, 32]);
     }
 
